35dc70841d193c4b83960f3f157c50ec57fb25cc
kailiang-4526 <phankailiang3@gmail.com>
2026-04-28 15:52:37 +1000

    Add files via upload
    python script for processing stm

---
+++ week7_lab.py
@@ -0,0 +1,41 @@
+import serial
+import time
+import wave
+import numpy as np
+
+# Setup
+port = "COM4"
+baud = 115200
+sample_rate = 12000
+duration = int(input("How many seconds of audio to record? "))
+total_samples = duration * sample_rate
+audio = []
+ser = serial.Serial(port, baud, timeout=5)
+
+print(f"Recording {duration} seconds...")
+time.sleep(1) # Give the port time to initialize
+ser.reset_input_buffer()
+
+# Optimization: Read the entire block at once rather than byte-by-byte
+
+for  i in range(total_samples):
+    raw_data = ser.read(size=1)
+    if raw_data:
+        audio.append(raw_data[0])
+
+if len(raw_data) < total_samples:
+    print(f"Warning: Only captured {len(raw_data)} samples.")
+data = np.array(audio)
+data = (data-data.min())/data.max()
+data = data*255
+data = data.astype(np.uint8)
+
+# Export to WAV
+with wave.open('output.wav', 'wb') as wf:
+    wf.setnchannels(1)      # Mono
+    wf.setsampwidth(1)      # 8-bit (1 byte per sample)
+    wf.setframerate(sample_rate)
+    wf.writeframes(data.tobytes())
+
+print("Done! Saved to output.wav")
+ser.close()
